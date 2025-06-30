24ceeaa312e8e0db19cb7427d72c8fb8782732d5
Leo Wu <wutsungtse@hotmail.com.tw>
2025-06-30 21:24:59 +0800

    edit

--- themes/proxio/config.js
+++ themes/proxio/config.js
@@ -159,18 +159,18 @@ const CONFIG = {
     // 关于作者区块
     PROXIO_ABOUT_ENABLE: true, // 关于作者区块区块开关
     PROXIO_ABOUT_TITLE: '關於作者',
-    PROXIO_ABOUT_TEXT_1: 'I am an Architect Turned Into a Product Designer',
+    PROXIO_ABOUT_TEXT_1: '我是薛丁格的工程師',
     PROXIO_ABOUT_TEXT_2:
-        'With a background in architecture, I now apply my expertise to product design, blending aesthetics, functionality, and innovation. My goal is to create modern, user-focused designs that bring your vision to life.',
+        '甚麼都會，有時卻甚麼也不會',
     PROXIO_ABOUT_PHOTO_URL: '/avatar.png',
-    PROXIO_ABOUT_KEY_1: '经验年限',
-    PROXIO_ABOUT_VAL_1: '10年+',
-    PROXIO_ABOUT_KEY_2: '客户',
-    PROXIO_ABOUT_VAL_2: '1000+',
-    PROXIO_ABOUT_KEY_3: '交付项目',
-    PROXIO_ABOUT_VAL_3: '5000+',
-    PROXIO_ABOUT_KEY_4: '累积创作时长（小时）',
-    PROXIO_ABOUT_VAL_4: '10000+',
+    PROXIO_ABOUT_KEY_1: '海外年限',
+    PROXIO_ABOUT_VAL_1: '6年',
+    PROXIO_ABOUT_KEY_2: '身高',
+    PROXIO_ABOUT_VAL_2: '174cm',
+    PROXIO_ABOUT_KEY_3: '體重',
+    PROXIO_ABOUT_VAL_3: '76kg',
+    PROXIO_ABOUT_KEY_4: '夢想',
+    PROXIO_ABOUT_VAL_4: '無窮大',
 
     PROXIO_ABOUT_BUTTON_URL: '/about',
     PROXIO_ABOUT_BUTTON_TEXT: '關於我',
